70695af01d36c58f79848adddab51826eb9efcd2
melitacasola <melissacasola@gmail.com>
2024-01-12 12:03:24 +0100

    api ClowsCard id=53 - id=54

--- src/app/choose-card/page.jsx
+++ src/app/choose-card/page.jsx
@@ -1,21 +1,27 @@
+'use client'
 import ButtonComponent from "../components/ButtonComponent/ButtonComponent";
-
-
-
+import CardDown from "../components/CardsComponents/CardDown";
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 export default function chooseCard() {
+    const router = useRouter()
+    
     return(
         <>
         <aside className="grid grid-rows grid-flow-col gap-4">
-            <div className="flex flex-col mx-10 pt-8">     
+            <div className="flex flex-col mx-10 pt-8">  
+            <Link href="/" ><ButtonComponent text={'Instrucciones'} className='w-full col-span-2 px-8 py-1 m-2' type="button"  />
+            </Link>   
             <ButtonComponent to={'/'} text={'Barajar'} className='col-span-2' />
-            <ButtonComponent to={'/'} text={'Instrucciones'} className='col-span-2'/>
+            
             </div>
             <div className="justify-center pt-10 col-span-4">
                 <h2 className={`text-[5.25rem] text-font-color`}>Escoge tu carta</h2>
             </div>
         </aside>
+        {/* <CardDeck/> */}
         
-        
+        <CardDown/>
         </>
     )
 
--- src/app/components/ButtonComponent/ButtonComponent.jsx
+++ src/app/components/ButtonComponent/ButtonComponent.jsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
 
 export default function ButtonComponent({to, text}) {
     return (
-        <button className="text-[1.875rem] text-font-color border-solid border-2 border-font-color rounded-3xl hover:shadow-lg px-8 py-1 m-2" to={to}>
+        <button className="flex flex-row items-center text-[1.875rem] text-font-color border-solid border-2 justify-evenly border-font-color rounded-3xl hover:shadow-lg px-8 py-1 m-2" to={to}>
             {text}
+            <Image 
+            src='/icons/tabler_arrows-shuffle.svg'
+            width='30'
+            height='30'
+            alt="suffle"/>
         </button>
     )
 }
