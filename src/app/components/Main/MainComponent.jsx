9047fa49d71e3db4a3818769f337d5745e256490
AliciaoE <alialioh@gmail.com>
2024-01-14 03:03:13 +0100

    add style cm

--- src/app/components/Main/MainComponent.jsx
+++ src/app/components/Main/MainComponent.jsx
@@ -3,9 +3,9 @@ import ButtonComponent from "../ButtonComponent/ButtonComponent";
 
 export default function MainComponent() {
   return (
-    <div >
-      <nav className="flex items-center gap-40 ">
-        <Link href="/choose-card" className="mb-11" >
+    <div>
+      <nav className="flex items-center gap-40 mb-11">
+        <Link href="/choose-card">
           <ButtonComponent text={"Empezar"} />
         </Link>
         <div className="items-center mt-10">
@@ -13,10 +13,10 @@ export default function MainComponent() {
           <h2 className={`text-[3rem] text-center text-font-color mt-5`}>Instrucciones</h2>
         </div>
       </nav>
-      <section className="grid grid-flow-col auto-cols-max gap-20 ">
-        <img src="/img/pngwing.png" className="w-13.188 h-140 mb-20 mt-10" />
-        <section className="gap-5 ">
-          <div className=" max-w-xl ml-25  p-5 ">
+      <section className="grid grid-flow-col auto-cols-max gap-20 mt-10 ">
+        <img src="/img/pngwing.png" className="w-13.188 h-140 mb-20" />
+        <section className="gap-5 max-w-xl ml-25">
+          <div className="p-5 cm:flex cm:colunm">
             <p className="text-[1.5em] text-font-color">
               ¡Bienvenido a nuestra plataforma de tarot! Descubre las
               revelaciones del pasado, presente y futuro con las cartas clown.
@@ -24,8 +24,8 @@ export default function MainComponent() {
               Explora las cartas y elige las 3 que más te llame la atención, con esas 3 cartas veremos tu pasado, presente y futuro.
             </p>
           </div>
-          <div className="max-w-xl ml-25  ">
-            <p className="text-[1.5rem] text-font-color p-6 mb-20">
+          <div className="mb-20">
+            <p className="text-[1.5rem] text-font-color p-6">
               Te aconsejamos que te siéntes en un lugar tranquilo, respira
               profundamente y permite que la energía de las cartas te envuelva.
               ¡La experiencia de Tarot de Sakura está diseñada para iluminar tu
