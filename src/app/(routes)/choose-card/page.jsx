9136a6555b08b593fb7623bf94c87d484625038e
kametazin <0x0e0w@gmail.com>
2024-01-17 23:00:38 +0100

    feat div h2 gap xl

--- src/app/(routes)/choose-card/page.jsx
+++ src/app/(routes)/choose-card/page.jsx
@@ -8,7 +8,7 @@ export default function chooseCard() {
 
     return (
         <main className="my-4 flex flex-col items-center shadow-sm overflow-hidden relative flex min-h-[800px] items-stretch">
-            <div className="flex flex-col md:flex-row items-center gap-40">
+            <div className="flex flex-col md:flex-row items-center xl:gap-40">
                 <ButtonsRendered />
                 <h2 className="text-center text-8xl max-md:max-w-full max-md:text-5xl">Escoge tu carta</h2>
             </div>
