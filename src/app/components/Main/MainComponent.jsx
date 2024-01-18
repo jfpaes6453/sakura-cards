a7ea3009107347d344bf9baa4bf05b3cd97bcb71
AliciaoE <alialioh@gmail.com>
2024-01-18 13:11:43 +0100

    we create final styles

--- src/app/components/Main/MainComponent.jsx
+++ src/app/components/Main/MainComponent.jsx
@@ -21,7 +21,7 @@ export default function MainComponent() {
         </nav>
         <section className="grid grid-flow-col auto-cols-max gap-20">
           <img
-            src="/img/pngwing.png"
+            src="/img/pngwing - copia.png"
             className="w-13.188 h-140 mb-20 mt-10"
             alt="Tarot"
           />
@@ -48,32 +48,25 @@ export default function MainComponent() {
           </section>
         </section>
       </div>
+
       {/* Mobile Version */}
-      <div className="block sm:hidden text-center">
-        <h1 className="text-3xl text-font-color leading-9 mt-5">
-          Sakura Tarot{" "}
+      <div className="block sm:hidden text-center mt-0 p-10">
+        <h1 className="  text-[5.25rem]  text-font-color leading-11  ">
+          Sakura tarot
         </h1>
-        <h2 className="text-2xl text-font-color mt-2">Instrucciones</h2>
-        <img
-          src="/img/pngwing.png"
-          alt="Tarot"
-          className="w-3/4 md:w-full h-auto mx-auto mb-4 mt-4"
+        <h2 className="text-[3rem] text-font-color mt-2 "> Instrucciones</h2>
+        <img src="/img/pngwing - copia.png"  alt="Tarot"
+          className="w-3/4 md:w-full h-auto mx-auto mb-10 mt-10"
         />
-        <p className="text-lg text-font-color mb-4">
-          ¡Bienvenido a nuestra plataforma de tarot! Descubre las revelaciones
-          del pasado, presente y futuro con las cartas clown. Sigue estos
-          sencillos pasos para obtener tu lectura personalizada. Explora las
-          cartas y elige las 3 que más te llame la atención, con esas 3 cartas
-          veremos tu pasado, presente y futuro.
+        <p className=" text-font-color mb-4 text-[1.5rem] text-justify p-9" >
+        ¡Bienvenido a nuestra plataforma de tarot! Descubre las revelaciones
+  del pasado, presente y futuro con las cartas clown. Sigue estos
+  sencillos pasos para obtener tu lectura personalizada. Explora las
+  cartas y elige las 3 que más te llame la atención, con esas 3 cartas
+  podremos adentrarnos en tu pasado, presente y futuro.
         </p>
-        <p className="text-lg text-font-color p-6 mb-4">
-          Te aconsejamos que te sientes en un lugar tranquilo, respira
-          profundamente y permite que la energía de las cartas te envuelva. ¡La
-          experiencia de Tarot de Sakura está diseñada para iluminar tu camino
-          interior y ofrecerte perspectivas fascinantes! ¡Que las cartas guíen
-          tu viaje!
-        </p>
-        <div className="mx-auto">
+       
+        <div className="mx-auto flex items-center justify-center ">
           <Link href="/choose-card">
             <ButtonComponent text="Empezar" />
           </Link>
