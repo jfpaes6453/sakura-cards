ea9a71acfc3762b85488768eddd7c934e2434a78
tephyxp <lindacespedesg@gmail.com>
2024-01-22 13:10:50 +0100

    adding font-awesome

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -6,14 +6,14 @@ import Footer from "@/app/components/Footer/Footer";
 
 describe('Footer', () => {
     test('renders social networks and copyright text', () => {
-      render(<Footer />);
-  
-      // Verificar que el componente SocialNetworks esté presente en el DOM
-      const socialNetworks = screen.getByTestId('social-networks');
-      expect(socialNetworks).toBeInTheDocument();
-  
-      // Verificar que el texto de copyright esté presente en el DOM
-      const copyrightText = screen.getByText(/© Copyright 2024 Clow Blossom Oracle/i);
-      expect(copyrightText).toBeInTheDocument();
+        render(<Footer />);
+
+        // Verificar que el componente SocialNetworks esté presente en el DOM
+        const socialNetworks = screen.getByTestId('social-networks');
+        expect(socialNetworks).toBeInTheDocument();
+
+        // Verificar que el texto de copyright esté presente en el DOM
+        const copyrightText = screen.getByText(/© Copyright 2024 Clow Blossom Oracle/i);
+        expect(copyrightText).toBeInTheDocument();
     });
-  });
+});
