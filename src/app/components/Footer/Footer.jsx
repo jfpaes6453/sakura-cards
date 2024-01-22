5fb5bfd8b64c603c49feb4128672b5adaa749f18
Gabi <hgpc314@gmail.com>
2024-01-22 01:15:43 +0100

    Buttons added to footer

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,12 +1,17 @@
 
 import SocialNetworks from '../SocialNetworks/SocialNetworks'
+import { FaHome, FaHistory } from "react-icons/fa";
 
 export default function Footer() {
-    
-    return(
+
+    return (
         <footer className="flex items-center flex-col md:flex-row justify-between md:border-t md:p-[33px]">
-            <SocialNetworks/>
-            <p className='text-2xl text-center px-8 py-8 md:py-0'>© Copyright 2024 Clow Blossom Oracle</p>
+            <SocialNetworks />
+            <section className="flex items-center gap-8 text-4xl">           
+                <a href="/"><FaHome /></a>
+                <a href="/results-history"><FaHistory /></a>
+            </section>
+            <p className='text-2xl text-center px-8 py-8 md:py-0'>© Copyright 2024 Sakura Tarot</p>
         </footer>
     )
 }
